c9f4035c5964f6bb6b65c560af3169e4945203ff
MATSUO-JUNTA <junta0527@icloud.com>
2024-07-31 23:51:24 +0900

    commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,13 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    @user = login(params[:email], params[:password])
+    if @user
+      redirect_back_or_to root_path, notice: ''
+    else
+      render :new
+    end
+  end
 end
