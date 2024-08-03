66e4dda244c34ff6c78f890f2a119a582fd02670
MATSUO-JUNTA <junta0527@icloud.com>
2024-08-03 21:32:21 +0900

    テストデータの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts
+
   authenticates_with_sorcery!
   validates :email, presence: true, uniqueness: true
   validates :password, length: { minimum: 6 }, confirmation: true
