dda95a405cda5e2f32f7047733ed8c1dbf5e608a
MATSUO-JUNTA <junta0527@icloud.com>
2024-08-11 21:03:15 +0900

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   has_many :posts
 
   authenticates_with_sorcery!
+
+  has_many :posts
+  
   validates :email, presence: true, uniqueness: true
   validates :password, length: { minimum: 6 }, confirmation: true
   validates :password_confirmation, presence: true
