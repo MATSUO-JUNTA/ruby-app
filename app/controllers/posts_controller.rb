ac9e0e9e25a3882c00426ae557f6cf4b5894daac
MATSUO-JUNTA <junta0527@icloud.com>
2024-08-16 00:17:40 +0900

    投稿の削除機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
-  before_action :require_login, only: [:new, :create, :edit, :update]
-  before_action :set_post, only: [:edit, :update]
+  before_action :require_login, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_post, only: [:edit, :update, :destroy]
+  before_action :authorize_post, only: [:destroy]
   def index
     @posts = Post.all.order(created_at: :desc)
   end
@@ -23,32 +24,22 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> c970b9a (投稿の編集機能)
   def edit
   end
 
   def update
     if @post.update(post_params)
-<<<<<<< HEAD
       redirect_to root_path, notice: '更新が成功しました'
     else
       render :edit, alert: '更新に失敗しました'
     end
   end
 
-=======
->>>>>>> 1ea2695 (投稿の詳細表示)
-=======
-      redirect_to posts_path, notice: '投稿が更新されました'
-    else
-      render :edit
-    end
+  def destroy
+    @post.destroy
+    redirect_to root_path, notice: '削除が成功しました'
   end
 
->>>>>>> c970b9a (投稿の編集機能)
   private
 
   def post_params
@@ -56,14 +47,12 @@ class PostsController < ApplicationController
   end
 
   def set_post
-<<<<<<< HEAD
     @post = current_user.posts.find(params[:id])
-=======
-    @post = Post.find(params[:id])
   end
 
-  def authorize_user
-    redirect_to posts_path, alert: '権限がありません' unless @post.user == current_user
->>>>>>> c970b9a (投稿の編集機能)
+  def authorize_post
+    unless @post.user == current_user
+      redirect_to root_path, notice: '他のユーザーが作成した投稿のため削除できません'
+    end
   end
 end
