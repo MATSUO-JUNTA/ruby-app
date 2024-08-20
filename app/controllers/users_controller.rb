6ee47ae080c56ad018f72cd93aced41bbe83e1f8
MATSUO-JUNTA <junta0527@icloud.com>
2024-08-21 03:08:52 +0900

    commit1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,22 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = current_user
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: 'プロフィールが更新されました'
+    else
+      render :edit
+    end
+  end
+
   private
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation, :avatar)
