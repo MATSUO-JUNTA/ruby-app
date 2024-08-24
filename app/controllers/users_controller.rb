34dd8cf7e7c6f0f6c9ca5cac1a05d6ecf121e1f5
MATSUO-JUNTA <junta0527@icloud.com>
2024-08-25 01:59:22 +0900

    commit2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,7 +44,6 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = current_user
   end
 
   def update
@@ -56,6 +55,7 @@ class UsersController < ApplicationController
   end
 
   private
+
   def set_user
     @user = User.find(params[:id])
   end
